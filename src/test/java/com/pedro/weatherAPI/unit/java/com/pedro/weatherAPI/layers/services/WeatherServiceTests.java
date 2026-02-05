f66f7ad8789ed5da4146fc8e2db98c6e83433ae6
pedro <pedrobrunigavioli@gmail.com>
2026-02-05 18:46:14 +0000

    test: updated weather service tests

--- src/test/java/com/pedro/weatherAPI/unit/java/com/pedro/weatherAPI/layers/services/WeatherServiceTests.java
+++ src/test/java/com/pedro/weatherAPI/unit/java/com/pedro/weatherAPI/layers/services/WeatherServiceTests.java
@@ -4,12 +4,14 @@ import com.pedro.weatherAPI.layers.domain.model.Day;
 import com.pedro.weatherAPI.layers.domain.model.WeatherRequest;
 import com.pedro.weatherAPI.layers.domain.model.WeatherResponse;
 import com.pedro.weatherAPI.layers.services.WeatherService;
+import com.pedro.weatherAPI.others.DateValidator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +20,9 @@ import tools.jackson.databind.ObjectMapper;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class WeatherServiceTests {
@@ -44,19 +46,25 @@ public class WeatherServiceTests {
         weatherRequest = new WeatherRequest("aa", LocalDate.of(2026,01,01),LocalDate.of(2026, 01, 05));
         weatherResponse = new WeatherResponse("a", days);
         jsonResponse ="json";
+
     }
 
     @Test
     public void getWeather_ValidWeatherRequest_ReturnsWeatherResponse(){
         when(restTemplate.getForObject(Mockito.anyString(), Mockito.eq(String.class))).thenReturn(jsonResponse);
         when(mapper.readValue(jsonResponse, WeatherResponse.class)).thenReturn(weatherResponse);
+        try (MockedStatic<DateValidator> mocked =
+                     mockStatic(DateValidator.class)) {
+            WeatherResponse response = service.getWeather(weatherRequest);
 
-        WeatherResponse response = service.getWeather(weatherRequest);
-
-        Assertions.assertThat(response).isNotNull();
-        verify(restTemplate).getForObject(Mockito.anyString(), Mockito.eq(String.class));
-        verify(mapper).readValue(jsonResponse, WeatherResponse.class);
+            Assertions.assertThat(response).isNotNull();
+            verify(restTemplate).getForObject(Mockito.anyString(), Mockito.eq(String.class));
+            verify(mapper).readValue(jsonResponse, WeatherResponse.class);
+            mocked.verify(() ->{
+                DateValidator.validateNotFutureDate(weatherRequest.getStartDate());
+                DateValidator.validateNotFutureDate(weatherRequest.getEndDate());
+                DateValidator.validateEndAfterStart(weatherRequest.getStartDate(), weatherRequest.getEndDate());
+            } );
+        }
     }
-
-
 }
